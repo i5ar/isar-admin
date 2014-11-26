2e51591c1a9f00b3bc00bdefd0c64ca99080db0d
i5arHub <i5ar@live.it>
2014-11-26 20:30:39 +0100

    Editing licence

--- isar-custom-admin.php
+++ isar-custom-admin.php
@@ -1,11 +1,30 @@
 <?php
-/*
-Plugin Name: iSar Admin
-Plugin URI: https://github.com/i5ar/isar-custom-admin
-Description: Custom Admin styles and contents. Access to plug-in panel from the Setting sub-menu
-Version: 1.2
-Author: iSarDesign
-Author URI: http://isarch.it/about-me/
+/**
+ * Plugin Name: iSar Custom Admin
+ * Plugin URI: https://github.com/i5ar/isar-custom-admin
+ * Description: Custom Admin styles and contents. Access to plugin panel from the Setting sub-menu.
+ * Version: 1.3
+ * Author: iSar
+ * Author URI: http://isarch.it/about-me/
+ * Text Domain: isar-custom-admin
+ * Domain Path: /languages/
+ * License: GPL2
+ */
+ 
+/*  Copyright 2014 iSar (email : i5ar.project@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 /**
  * Languages
